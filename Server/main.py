c6634816cfb6ea787ae5dde9623bc69edbe6eb16
Martin Borek <mborekcz@lenovo.mborekcz>
2016-10-07 13:13:23 +0300

    flask request

--- Server/main.py
+++ Server/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -6,6 +7,14 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/form/')
+def form():
+    return request.query_string
+
+@app.route('/form2/')
+def form():
+    return request.url
 
 @app.errorhandler(404)
 def page_not_found(e):
