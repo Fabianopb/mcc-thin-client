e5a4a67368eae6f728d82d50812b9f9cb1a8ec09
Fabiano Brito <fabianopbrito@gmail.com>
2016-10-15 17:25:20 +0300

    Makes blank activity as the initial.

--- ThinClientG13-mobile/app/src/main/java/com/mccG13/MainActivity.java
+++ ThinClientG13-mobile/app/src/main/java/com/mccG13/MainActivity.java
@@ -1,11 +1,11 @@
 package com.mccG13;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.coboltforge.dontmind.multivnc.R;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
