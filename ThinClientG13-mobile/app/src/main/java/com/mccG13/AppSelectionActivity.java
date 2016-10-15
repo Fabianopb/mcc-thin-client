5f8b71d0bb5b7e7d32a02f7dd8a87320fc2af68c
Fabiano Brito <fabianopbrito@gmail.com>
2016-10-15 17:42:44 +0300

    Finally integrates the multiVNC to our app to have a native feel.

---
+++ ThinClientG13-mobile/app/src/main/java/com/mccG13/AppSelectionActivity.java
@@ -0,0 +1,32 @@
+package com.mccG13;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.coboltforge.dontmind.multivnc.R;
+
+public class AppSelectionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_selection);
+    }
+
+    public void startApplication(View view) {
+
+        String instanceIP = ""; // Put the instance IP here!!
+        String cloudPort = ":5901";
+        String colorScheme = "/C24bit";
+        String instancePwd = "/tReFre4r";
+
+        Intent intent = new Intent(this, com.coboltforge.dontmind.multivnc.VncCanvasActivity.class);
+        intent.setData(Uri.parse("vnc://" + instanceIP + cloudPort + colorScheme + instancePwd));
+        startActivity(intent);
+    }
+}
--- ThinClientG13-mobile/app/src/main/java/com/mccG13/MainActivity.java
+++ ThinClientG13-mobile/app/src/main/java/com/mccG13/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mccG13;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.coboltforge.dontmind.multivnc.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startLogin(View view) {
+        Intent intent = new Intent(this, AppSelectionActivity.class);
+        startActivity(intent);
     }
 }
